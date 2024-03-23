bcecebcd4aca01fbee32eb6ed172e49329abb089
vjdbarayuga <vjdbarayuga@gmail.com>
2024-03-24 01:22:31 +0800

    updated recently

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use(
     credentials: true,
     //react frontend
     //origin: 'http://localhost:3000',
-    origin: 'https://graduate-tracer-2024-331a.vercel.app/login',
+    origin: '*',
   })
 );
 
